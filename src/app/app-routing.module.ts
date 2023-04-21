e35a64b20a5595182e3c7034cebb32ac68dacc0b
Gonzalo0743 <gonzaloa0743@gmail.com>
2023-04-20 19:00:11 -0600

    Add Project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomePageComponent } from './Components/welcome-page/welcome-page.component';
+import { LoginAdminComponent } from './Components/login-admin/login-admin.component';
+import { LoginClientComponent } from './Components/login-client/login-client.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: WelcomePageComponent
+  },
+  {
+    path:'loginAdmin',
+    component: LoginAdminComponent
+  },
+  {
+    path:'loginClient',
+    component: LoginClientComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomePageComponent } from './Components/welcome-page/welcome-page.component';
+import { LoginAdminComponent } from './Components/login-admin/login-admin.component';
+import { LoginClientComponent } from './Components/login-client/login-client.component';
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomePageComponent,
+    LoginAdminComponent,
+    LoginClientComponent
   ],
   imports: [
     BrowserModule,
