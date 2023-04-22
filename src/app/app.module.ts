b60c35bcac3dba4227b4db0ad8251d5efb4e69dc
Gonzalo0743 <gonzaloa0743@gmail.com>
2023-04-22 15:07:04 -0600

    Updating All Web

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './Components/main-page/main-page.component';
+import { ContactComponent } from './Components/contact/contact.component';
+import { ProjectsComponent } from './Components/projects/projects.component';
+import { ResumeComponent } from './Components/resume/resume.component';
 
 
 
@@ -13,6 +17,10 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [
     AppComponent,
+    MainPageComponent,
+    ContactComponent,
+    ProjectsComponent,
+    ResumeComponent,
     
   ],
   imports: [
