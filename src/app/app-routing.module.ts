47f13fae0d3f372a91afb565afb19eb9b3b176b3
Gonzalo0743 <gonzaloa0743@gmail.com>
2023-04-23 17:03:51 -0600

    Updating page generally

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './Pages/main-page/main-page.component';
+import { LoginAdminComponent } from './Pages/login-admin/login-admin.component';
+import { RegisterClientComponent } from './Pages/register-client/register-client.component';
+import { RegisterAdminComponent } from './Pages/register-admin/register-admin.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path:'',
+    component: MainPageComponent
+  },
+  {
+    path:'loginAdmin',
+    component: LoginAdminComponent
+  },
+  {
+    path:'registerClient',
+    component: RegisterClientComponent
+  },
+  {
+    path:'registerAdmin',
+    component: RegisterAdminComponent
+  }
+  
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainPageComponent } from './Components/main-page/main-page.component';
-import { ContactComponent } from './Components/contact/contact.component';
-import { ProjectsComponent } from './Components/projects/projects.component';
-import { ResumeComponent } from './Components/resume/resume.component';
+import { TopNavBarComponent } from './Components/top-nav-bar/top-nav-bar.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { MainPageComponent } from './Pages/main-page/main-page.component';
+import { LoginAdminComponent } from './Pages/login-admin/login-admin.component';
+import { RegisterClientComponent } from './Pages/register-client/register-client.component';
+import { RegisterAdminComponent } from './Pages/register-admin/register-admin.component';
 
 
 
@@ -17,10 +19,13 @@ import { ResumeComponent } from './Components/resume/resume.component';
 @NgModule({
   declarations: [
     AppComponent,
+    TopNavBarComponent,
+    FooterComponent,
     MainPageComponent,
-    ContactComponent,
-    ProjectsComponent,
-    ResumeComponent,
+    LoginAdminComponent,
+    RegisterClientComponent,
+    RegisterAdminComponent,
+    
     
   ],
   imports: [
